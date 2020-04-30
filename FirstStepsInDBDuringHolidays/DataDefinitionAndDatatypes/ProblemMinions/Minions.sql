d4b1f7dffd932afaffbb3000eaa77f3818872027
slavi044 <slavi044@abv.bg>
2020-04-30 19:00:58 +0300

    Add homework

--- FirstStepsInDBDuringHolidays/DataDefinitionAndDatatypes/ProblemMinions/Minions.sql
+++ FirstStepsInDBDuringHolidays/DataDefinitionAndDatatypes/ProblemMinions/Minions.sql
@@ -16,7 +16,7 @@ Id INT PRIMARY KEY IDENTITY(1,1),
 ALTER TABLE Minions
 ADD TownId INT FOREIGN KEY REFERENCES Towns(Id)
 
-SET IDENTITY_INSERT Towns ON 
+SET IDENTITY_INSERT Towns OFF
 
 INSERT INTO Towns(Id, [Name])
 VALUES (1, 'Sofia'),
@@ -29,3 +29,32 @@ VALUES (1, 'Kevin', 22, 1),
 	   (3, 'Steward', NULL, 2)
 
 SET IDENTITY_INSERT Minions ON
+--Problems 1, 2, 3, 4
+
+--Problem 5
+TRUNCATE TABLE MINIONS
+
+--Problem 6
+DROP TABLE Minions
+DROP TABLE Towns
+
+--Problem 7
+CREATE TABLE People(
+Id INT PRIMARY KEY IDENTITY(1, 1),
+[Name] NVARCHAR(200) NOT NULL,
+Picture NVARCHAR(MAX),
+Height DECIMAL(15,2),
+[Weight] DECIMAL(15,2),
+Gender CHAR(1) NOT NULL,
+Birthdate DATE NOT NULL,
+Biography NVARCHAR(MAX)
+)
+
+SET IDENTITY_INSERT People ON
+
+INSERT INTO People(Id, [Name], Picture, Height, [Weight], Gender, Birthdate, Biography)
+VALUES (1, 'Gosho','asdqweasdasd', 1.79999, 100.01, 'm', '1984-02-29', 'I am Gosho'),
+	   (2, 'Gosho','asdqweasdasd', 1.79999, 100.01, 'm', '1984-02-29', 'I am Gosho'),
+	   (3, 'Gosho','asdqweasdasd', 1.79999, 100.01, 'm', '1984-02-29', 'I am Gosho'),
+	   (4, 'Gosho','asdqweasdasd', 1.79999, 100.01, 'm', '1984-02-29', 'I am Gosho'),
+	   (5, 'Gosho','asdqweasdasd', 1.79999, 100.01, 'm', '1984-02-29', 'I am Gosho')
