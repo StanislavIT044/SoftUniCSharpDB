381dd1c7018cd70506deec086f8011dbbe0bafc6
slavi044 <slavi044@abv.bg>
2020-05-08 01:44:05 +0300

    Add homework

---
+++ FirstStepsInDBDuringHolidays/Built-inFunctions/SQLQueriesProblems12-13.sql
@@ -0,0 +1,15 @@
+USE [Geography]
+
+--Problem12
+SELECT CountryName, IsoCode
+  FROM Countries
+ WHERE CountryName LIKE '%a%a%a%'
+ORDER BY IsoCode
+
+--Problem13
+SELECT PeakName, 
+	   RiverName, 
+	   LOWER(PeakName + SUBSTRING(RiverName, 2, LEN(RiverName))) AS Mix
+  FROM Peaks, Rivers
+ WHERE RIGHT(PeakName, 1) = LEFT(RiverName,1) 
+ORDER BY Mix
