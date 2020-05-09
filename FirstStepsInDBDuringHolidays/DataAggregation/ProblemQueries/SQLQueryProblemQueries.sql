ea48f6af6932181a0657983c5457434a14af38d4
slavi044 <slavi044@abv.bg>
2020-05-09 16:51:57 +0300

    Add homework

---
+++ FirstStepsInDBDuringHolidays/DataAggregation/ProblemQueries/SQLQueryProblemQueries.sql
@@ -0,0 +1,63 @@
+USE Gringotts
+
+--Problem01
+SELECT COUNT(*) AS [Count]
+  FROM WizzardDeposits
+
+--Problem02
+SELECT MAX(MagicWandSize) AS LongestMgicWand 
+  FROM WizzardDeposits
+
+--Problem03
+  SELECT DepositGroup, MAX(MagicWandSize) AS LongestMagicWand
+    FROM WizzardDeposits
+GROUP BY DepositGroup
+
+--Problem04
+SELECT TOP(2) DepositGroup
+  FROM WizzardDeposits
+GROUP BY DepositGroup
+ORDER BY AVG(MagicWandSize)
+
+--Problem05
+SELECT DepositGroup, SUM(DepositAmount) AS TotalDepositAmount
+  FROM WizzardDeposits
+GROUP BY DepositGroup
+
+--Problem06
+SELECT DepositGroup, SUM(DepositAmount)
+  FROM WizzardDeposits
+ WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup
+
+--Problem07
+SELECT DepositGroup, SUM(DepositAmount) AS TotalSum
+  FROM WizzardDeposits
+ WHERE MagicWandCreator = 'Ollivander family'
+GROUP BY DepositGroup
+HAVING SUM(DepositAmount) < 150000
+ORDER BY TotalSum DESC
+
+--Problem08
+SELECT DepositGroup, MagicWandCreator, MIN(DepositCharge) AS MinDepositCharge
+  FROM WizzardDeposits
+GROUP BY DepositGroup, MagicWandCreator
+ORDER BY MagicWandCreator, DepositGroup
+
+--Problem09
+SELECT AgeGroupTable.AgeGroup, COUNT(AgeGroupTable.AgeGroup)
+  FROM (
+SELECT Age, 
+  CASE 
+  WHEN Age BETWEEN 0 AND 10 THEN '[0-10]'
+  WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+  WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+  WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+  WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+  WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+  ELSE '[61+]'
+   END AS AgeGroup
+  FROM WizzardDeposits
+) AS AgeGroupTable
+GROUP BY AgeGroupTable.AgeGroup
+--38
