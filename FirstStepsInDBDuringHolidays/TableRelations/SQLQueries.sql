4b19447cdce00a354ed53dce8b8feddc3d3b3c5f
slavi044 <slavi044@abv.bg>
2020-05-12 18:24:00 +0300

    Add homework

--- FirstStepsInDBDuringHolidays/TableRelations/SQLQueries.sql
+++ FirstStepsInDBDuringHolidays/TableRelations/SQLQueries.sql
@@ -35,3 +35,76 @@ INSERT INTO Persons (PersonID, FirstName, Salary, PassportID) VALUES
 (1, 'Roberto', 43300.00, 102),
 (2, 'Tom', 56100.00, 103),
 (3, 'YaFGna', 60200.00, 101)
+
+
+--Problem02
+CREATE TABLE Manufacturers
+(
+	ManufacturerID INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(15) NOT NULL,
+	EstablishedOn DATE NOT NULL
+)
+
+CREATE TABLE Models
+(
+	ModelID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] VARCHAR(15) NOT NULL,
+	ManufacturerID INT FOREIGN KEY REFERENCES Manufacturers(ManufacturerID)
+)
+
+SET IDENTITY_INSERT Manufacturers OFF
+
+INSERT INTO Manufacturers (ManufacturerID, [Name], EstablishedOn) VALUES
+	(1, 'BMW', '07/03/1916'),
+	(2, 'Tesla', '01/01/2003'),
+	(3, 'Lada', '01/05/1966')
+
+INSERT INTO Models ([Name], ManufacturerID) VALUES
+	('X1', 1),
+	('i6', 1),
+	('Model S', 2),
+	('Model X', 2),
+	('Model 3', 2),
+	('Nova', 3)
+
+SELECT * FROM Models
+
+--Problem03 
+CREATE TABLE Students
+(
+	StudentID INT,
+	[Name] VARCHAR(20)
+)
+
+CREATE TABLE Exams
+(
+	ExamID INT,
+	[Name] NVARCHAR(20)
+)
+
+CREATE TABLE StudentsExams
+(
+	StudentID INT,
+	ExamID INT
+)
+
+ALTER TABLE StudentsExams
+ALTER COLUMN StudentID INT NOT NULL
+ALTER COLUMN ExamID INT NOT NULL
+
+ALTER TABLE StudentsExams
+ADD CONSTRAINT PK_StudentsExams PRIMARY KEY (StudentID, ExamID)
+
+
+ALTER TABLE Students
+ALTER COLUMN StudentID INT NOT NULL
+
+ALTER TABLE Students
+ADD CONSTRAINT PK_Students PRIMARY KEY (StudentID)
+
+
+ALTER TABLE Exams
+ALTER COLUMN ExamID INT NOT NULL
+
+ALTER TABLE Exams
+ADD CONSTRAINT PK_Exams PRIMARY KEY (ExamID)
