bde7747ea2d4e3c0bf43d2e1640fed0cc6a761fe
slavi044 <slavi044@abv.bg>
2020-05-28 22:16:40 +0300

    Add homework

---
+++ 03TableRelations/Problem03SQLQueries.sql
@@ -0,0 +1,35 @@
+USE TestDB
+
+CREATE TABLE Students(
+	StudentID INT PRIMARY KEY IDENTITY(1, 1),
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Exams(
+	ExamID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE StudentsExams(
+	StudentID INT FOREIGN KEY REFERENCES Students(StudentID),
+	ExamID INT FOREIGN KEY REFERENCES Exams(ExamID),
+	CONSTRAINT PK_ExamIdAndName PRIMARY KEY (StudentID, ExamID)
+)
+
+INSERT INTO Students VALUES
+('Mila'),
+('Toni'),
+('Ron')
+
+INSERT INTO Exams VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+INSERT INTO StudentsExams VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103)
