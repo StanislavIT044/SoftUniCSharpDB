debaeb82c2191a1c6bfe6d405abd9541c2b8e6e7
slavi044 <slavi044@abv.bg>
2020-06-02 00:30:51 +0300

    Add homework

--- 04Built-InFunctions/SoftUniDBSQLQueries.sql
+++ 04Built-InFunctions/SoftUniDBSQLQueries.sql
@@ -25,3 +25,43 @@ SELECT [Name]
   FROM Towns
  WHERE LEN([Name]) BETWEEN 5 AND 6 
 ORDER BY [Name]
+
+--Problem06
+SELECT TownID, [Name]
+  FROM Towns
+ WHERE [Name] LIKE '[MKBE]%'
+ORDER BY [Name]
+
+--Problem07
+SELECT TownID, [Name]
+  FROM Towns
+ WHERE [Name] NOT LIKE '[RBD]%'
+ORDER BY [Name]
+
+--Problem08
+CREATE VIEW V_EmployeesHiredAfter2000 AS
+SELECT FirstName, LastName
+  FROM Employees
+ WHERE YEAR(HireDate) > '2000'
+
+--Problem09
+SELECT FirstName, LastName
+  FROM Employees
+ WHERE LEN(LastName) = 5
+
+--Problem10
+SELECT EmployeeID, FirstName, LastName, Salary,
+	   DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS RANK
+  FROM Employees
+ WHERE Salary BETWEEN 10000 AND 50000
+ ORDER BY Salary DESC
+
+--Problem11
+SELECT *
+  FROM (
+		SELECT EmployeeID, FirstName, LastName, Salary,
+			DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS RANK
+		FROM Employees
+		WHERE Salary BETWEEN 10000 AND 50000) AS NewTable
+ WHERE Rank = 2
+ ORDER BY Salary DESC
