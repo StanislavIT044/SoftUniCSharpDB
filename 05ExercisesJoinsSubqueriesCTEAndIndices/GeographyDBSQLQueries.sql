3fcee8ec1170a94388666b62f604d5ec5610045e
slavi044 <slavi044@abv.bg>
2020-06-07 13:26:27 +0300

    Add homework

--- 05ExercisesJoinsSubqueriesCTEAndIndices/GeographyDBSQLQueries.sql
+++ 05ExercisesJoinsSubqueriesCTEAndIndices/GeographyDBSQLQueries.sql
@@ -14,3 +14,34 @@ SELECT CountryCode, COUNT(m.MountainRange) AS MountainRanges
   JOIN MountainsCountries AS mc ON mc.MountainId = m.Id
  WHERE mc.CountryCode IN ('BG', 'RU', 'US')
  GROUP BY mc.CountryCode
+
+--Problem14
+SELECT TOP(5) c.CountryName, r.RiverName
+  FROM Countries AS c
+  LEFT JOIN CountriesRivers AS cr ON cr.CountryCode = c.CountryCode
+  LEFT JOIN Rivers AS r ON cr.RiverId = r.Id
+ WHERE ContinentCode = 'AF'
+ ORDER BY CountryName
+
+--Problem15
+SELECT ContinentCode, CurrencyCode, CurrencyCount AS CurrencyUsage
+  FROM (
+	   SELECT ContinentCode, 
+       CurrencyCode,
+	   CurrencyCount,
+	   DENSE_RANK() OVER
+	   (PARTITION BY ContinentCode ORDER BY CurrencyCount DESC) AS CurrencyRank
+	   FROM (
+		       SELECT ContinentCode,
+		       	   CurrencyCode,
+		       	   COUNT(*) AS [CurrencyCount]
+		         FROM Countries
+		        GROUP BY ContinentCode, CurrencyCode
+				) AS [CurrencyCountQuery]
+ WHERE CurrencyCount > 1 ) AS CurrencyRanking
+ WHERE CurrencyRank = 1
+ ORDER BY ContinentCode 
+
+--Problem16
+SELECT * 
+  FROM MountainsCountries
