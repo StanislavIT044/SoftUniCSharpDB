97fd6e6d341f90d0a627e946c485328efc884183
slavi044 <slavi044@abv.bg>
2020-06-09 02:14:45 +0300

    Add homework

--- 06DataAggregation/GringottsDBSQLQueries.sql
+++ 06DataAggregation/GringottsDBSQLQueries.sql
@@ -37,3 +37,31 @@ SELECT DepositGroup, SUM(DepositAmount) AS TotalSum
  GROUP BY DepositGroup 
 HAVING SUM(DepositAmount) < 150000
  ORDER BY TotalSum DESC
+
+--Problem08
+SELECT DepositGroup, MagicWandCreator, MIN(DepositCharge) AS MinDepositCharge
+  FROM WizzardDeposits
+ GROUP BY DepositGroup, MagicWandCreator
+ ORDER BY MagicWandCreator, DepositGroup
+
+--Problem09
+SELECT AgeGroupTable.AgeGroup, COUNT(AgeGroupTable.AgeGroup)
+  FROM (
+SELECT Age,
+    CASE
+    WHEN Age <= 10 THEN '[0-10]'
+	WHEN Age <= 20 THEN '[11-20]'
+	WHEN Age <= 30 THEN '[21-30]'
+	WHEN Age <= 40 THEN '[31-40]'
+	WHEN Age <= 50 THEN '[41-50]'
+	WHEN Age <= 60 THEN '[51-60]'
+	ELSE '[61+]' 
+   END AS AgeGroup
+  FROM WizzardDeposits) AS AgeGroupTable
+ GROUP BY AgeGroupTable.AgeGroup
+
+--Problem10
+SELECT DISTINCT SUBSTRING(FirstName, 1, 1) AS FirstLetter
+  FROM WizzardDeposits
+ WHERE DepositGroup = 'Troll Chest'
+ GROUP BY FirstName
