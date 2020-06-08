8e742dd6b5b0ac8c7bb650eddf13b8a14b54d5f1
slavi044 <slavi044@abv.bg>
2020-06-08 23:20:59 +0300

    Add homework

--- FirstStepsInDBDuringHolidays/ExamPrep/Prep01/SQLQueries.sql
+++ FirstStepsInDBDuringHolidays/ExamPrep/Prep01/SQLQueries.sql
@@ -47,3 +47,17 @@ CREATE TABLE Tickets(
 	Price DECIMAL(15, 2) NOT NULL
 )
 
+--Problem02
+INSERT INTO Planes VALUES
+('Airbus 336', 112, 5132),
+('Airbus 330', 432, 5325),
+('Boeing 369', 231, 2355),
+('Stelt 297', 254, 2143),
+('Boeing 338', 165, 5111),
+('Airbus 558', 387, 1342),
+('Boeing 128', 345, 5541)
+
+INSERT INTO [LuggageTypes] VALUES
+('Crossbody Bag'),
+('School Backpack'),
+('Shoulder Bag')
