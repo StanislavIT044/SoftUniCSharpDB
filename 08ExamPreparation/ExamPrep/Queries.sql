1730ae092e6409f0ee15caa3dc077ef0c29908f5
slavi044 <slavi044@abv.bg>
2020-06-20 01:03:46 +0300

    Add homework

--- 08ExamPreparation/ExamPrep/Queries.sql
+++ 08ExamPreparation/ExamPrep/Queries.sql
@@ -21,3 +21,55 @@ WHERE CloseDate IS NULL
 SELECT [Description], FORMAT(OpenDate, 'dd-MM-yyyy')
   FROM Reports
  ORDER BY Reports.OpenDate ASC, Description ASC
+
+--Problem05
+SELECT [Description], FORMAT(OpenDate, 'dd-MM-yyyy')
+  FROM Reports
+ WHERE EmployeeId IS NULL
+ORDER BY Reports.OpenDate ASC, Description ASC
+
+--Problem06
+SELECT r.[Description], c.[Name]
+  FROM Reports AS r
+  JOIN Categories AS c ON c.Id = r.CategoryId
+ ORDER BY [Description] ASC, [Name] ASC
+
+--Problem07
+SELECT TOP(5) c.[Name], COUNT(c.Id) AS ReportsNumber
+  FROM Reports AS r
+  JOIN Categories AS c ON r.CategoryId = c.Id
+ GROUP BY c.Id, c.[Name]
+ ORDER BY ReportsNumber DESC, c.[Name] ASC
+
+--Problem08
+SELECT u.Username, c.[Name] AS CategoryName
+  FROM Reports AS r
+  JOIN Users AS u ON r.UserId = u.Id
+  JOIN Categories AS c ON c.Id = r.CategoryId 
+ WHERE DATEPART(DAY, r.OpenDate) = DATEPART(DAY, u.Birthdate) AND
+	   DATEPART(MONTH, r.OpenDate) = DATEPART(MONTH, u.Birthdate) 
+ ORDER BY Username ASC, CategoryName ASC
+
+--Problem09
+SELECT e.FirstName + ' ' + LastName AS FullName,
+	   (SELECT COUNT(DISTINCT UserId) FROM Reports WHERE EmployeeId = e.Id) AS UsersCount
+  FROM Employees AS e
+ ORDER BY UsersCount DESC, FullName ASC
+
+--Problem11
+CREATE FUNCTION udf_HoursToComplete(@StartDate DATETIME, @EndDate DATETIME)
+RETURNS INT
+AS
+BEGIN
+	IF (@StartDate IS NULL)
+	BEGIN
+		RETURN 0;
+	END
+
+	IF (@EndDate IS NULL)
+	BEGIN
+		RETURN 0;
+	END
+
+	RETURN DATEDIFF(hour, @StartDate, @EndDate);
+END
