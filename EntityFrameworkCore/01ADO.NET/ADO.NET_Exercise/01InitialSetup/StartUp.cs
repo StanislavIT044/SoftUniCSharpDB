9d1ede3d8a108f2bd30551881b7b8a3f528cf996
slavi044 <slavi044@abv.bg>
2020-06-24 01:28:40 +0300

    Add homework

---
+++ EntityFrameworkCore/01ADO.NET/ADO.NET_Exercise/01InitialSetup/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _01InitialSetup
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            string connectionString = @"Server=.;Database=master;Integrated Security=true";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            using (connection)//44:40
+            {
+                string queryCreateDB = "CREATE DATABASE MinionsDB";
+
+                SqlCommand createDB = new SqlCommand(queryCreateDB, connection);
+
+                createDB.ExecuteNonQuery();
+            }
+        }
+    }
+}
