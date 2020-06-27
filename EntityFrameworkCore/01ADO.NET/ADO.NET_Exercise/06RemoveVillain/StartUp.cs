12c75add590b6c1698bae05a9c9da9879c389a69
slavi044 <slavi044@abv.bg>
2020-06-27 14:10:56 +0300

    Add homework

---
+++ EntityFrameworkCore/01ADO.NET/ADO.NET_Exercise/06RemoveVillain/StartUp.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _06RemoveVillain
+{
+    class StartUp
+    {
+        const string connectionString = @"Server=.;Database=MinionsDB;Integrated Security=true";
+
+        static void Main()
+        {
+            int villainId = int.Parse(Console.ReadLine());
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string villainName = ExistVillain(connection, villainId);
+
+                if (villainName != String.Empty)
+                {
+                    Console.WriteLine($"{villainName} was deleted.");
+
+                    DeleteMinions(connection, villainId);
+
+                    DeleteVillain(connection, villainId);
+                }
+                else
+                {
+                    Console.WriteLine("No such villain was found.");
+                }
+            }
+        }
+
+        private static void DeleteVillain(SqlConnection connection, int villainId)
+        {
+            string deleteVillainQuery = @"DELETE FROM Villains
+                                           WHERE Id = @villainId";
+
+            using (SqlCommand deleteVillain = new SqlCommand(deleteVillainQuery, connection))
+            {
+                deleteVillain.Parameters.AddWithValue("@villainId", villainId);
+
+                deleteVillain.ExecuteNonQuery();
+            }
+        }
+
+        private static void DeleteMinions(SqlConnection connection, int villainId)
+        {
+            string minionsCount;
+
+            string countMinionsQuery = @"SELECT COUNT(VillainId) AS num
+                                           FROM MinionsVillains
+                                          GROUP BY VillainId
+                                         HAVING VillainId = @villainId";
+
+            using (SqlCommand countMinions = new SqlCommand(countMinionsQuery, connection))
+            {
+                countMinions.Parameters.AddWithValue("@villainId", villainId);
+
+                using (SqlDataReader reader = countMinions.ExecuteReader())
+                {
+                    reader.Read();
+
+                    minionsCount = reader["num"].ToString();
+                }
+            }
+
+            string deleteMinionsQuery = @"DELETE FROM MinionsVillains 
+                                           WHERE VillainId = @villainId";
+
+            using (SqlCommand deleteMinions = new SqlCommand(deleteMinionsQuery, connection))
+            {
+                deleteMinions.Parameters.AddWithValue("@villainId", villainId);
+
+                deleteMinions.ExecuteNonQuery();
+            }
+
+            Console.WriteLine($"{minionsCount} minions were released.");
+        }
+
+        private static string ExistVillain(SqlConnection connection, int villainId)
+        {
+            string selectVillainQuery = @"SELECT Name FROM Villains WHERE Id = @villainId";
+
+            using (SqlCommand selectVillain = new SqlCommand(selectVillainQuery, connection))
+            {
+                selectVillain.Parameters.AddWithValue("@villainId", villainId);
+
+                using (SqlDataReader reader = selectVillain.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return reader["Name"]?.ToString();
+                    }
+
+                    return String.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ EntityFrameworkCore/01ADO.NET/ADO.NET_Exercise/07PrintAllMinionNames/StartUp.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace _07PrintAllMinionNames
+{
+    class StartUp
+    {
+        const string connectionString = @"Server=.;Database=MinionsDB;Integrated Security=true";
+
+        static void Main()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            
+            OrderNames(connection);
+        }
+
+        private static void OrderNames(SqlConnection connection)
+        {
+            List<string> initialOrderOfMinions = new List<string>();
+            List<string> arrangedOrderOfMinions = new List<string>();
+
+            using (connection)
+            {
+                connection.Open();
+
+                var command = new SqlCommand("SELECT Name FROM Minions", connection);
+
+                var reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    if (!reader.HasRows)
+                    {
+                        return;
+                    }
+
+                    while (reader.Read())
+                    {
+                        initialOrderOfMinions.Add(reader["Name"].ToString());
+                    }
+                }
+            }
+
+            while (initialOrderOfMinions.Count > 0)
+            {
+
+                arrangedOrderOfMinions.Add(initialOrderOfMinions[0]);
+                initialOrderOfMinions.RemoveAt(0);
+
+                if (initialOrderOfMinions.Count > 0)
+                {
+                    arrangedOrderOfMinions.Add(initialOrderOfMinions[initialOrderOfMinions.Count - 1]);
+                    initialOrderOfMinions.RemoveAt(initialOrderOfMinions.Count - 1);
+                }
+            }
+
+            foreach (var minion in arrangedOrderOfMinions)
+            {
+                Console.WriteLine(minion);
+            }
+        }
+    }
+}
