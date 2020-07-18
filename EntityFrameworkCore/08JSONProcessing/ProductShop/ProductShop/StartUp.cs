462fddcf09639e39e48b15fec0a8720fba569276
slavi044 <slavi044@abv.bg>
2020-07-18 17:14:03 +0300

    Add homework

--- EntityFrameworkCore/08JSONProcessing/ProductShop/ProductShop/StartUp.cs
+++ EntityFrameworkCore/08JSONProcessing/ProductShop/ProductShop/StartUp.cs
@@ -10,7 +10,54 @@ namespace ProductShop
 {
     public class StartUp
     {
-        public static void Main(string[] args)
+        public static void Main()
+        {
+            ProductShopContext db = new ProductShopContext();
+
+            //ResetDatabase(db);
+
+            //Problem01
+            //string inputJson = File.ReadAllText("../../../Datasets/users.json");
+            //Console.WriteLine(ImportUsers(db, inputJson));
+
+            //Problem02
+            //string inputJson = File.ReadAllText("../../../Datasets/products.json");
+            //Console.WriteLine(ImportProducts(db, inputJson));
+        }
+
+        private static void ResetDatabase(ProductShopContext db)
+        {
+            db.Database.EnsureDeleted();
+            Console.WriteLine("Database was successfully deleted!");
+
+            db.Database.EnsureCreated();
+            Console.WriteLine("Database was successfully created!");
+        }
+
+        //Problem01
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count}";
+        }
+
+        //Problem02
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+
+            context.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Count}";
+        }
+
+        //Problem03
+        public static string ImportCategories(ProductShopContext context, string inputJson)
         {
 
         }
