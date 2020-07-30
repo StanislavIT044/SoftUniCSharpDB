7fee82f28e8326e5d9adca5b63ffb526f62b1e28
slavi044 <slavi044@abv.bg>
2020-07-30 19:05:04 +0300

    Add ready database. Exam problem 01

--- EntityFrameworkCore/ExamPreparation/C#DBAdvancedExam-07Apr2019/Cinema/Cinema/Data/Models/Movie.cs
+++ EntityFrameworkCore/ExamPreparation/C#DBAdvancedExam-07Apr2019/Cinema/Cinema/Data/Models/Movie.cs
@@ -1,7 +1,37 @@
 ﻿namespace Cinema.Data.Models
 {
+    using System;
+    using Cinema.Data.Models.Enums;
+    using System.ComponentModel.DataAnnotations;
+    using System.Collections.Generic;
+
     public class Movie
     {
-        
+        public Movie()
+        {
+            this.Projections = new HashSet<Projection>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3), MaxLength(20)]
+        public string Title { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        public TimeSpan Duration  { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public double Rating { get; set; }
+
+        [Required]
+        [MinLength(3), MaxLength(20)]
+        public string Director { get; set; }
+
+        public ICollection<Projection> Projections  { get; set; }
     }
 }
