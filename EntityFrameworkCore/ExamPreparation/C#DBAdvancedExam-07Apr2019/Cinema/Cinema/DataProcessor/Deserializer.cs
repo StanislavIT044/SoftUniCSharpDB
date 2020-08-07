3396420b69a3670baab4ed1c0235dda09b0bb66e
slavi044 <slavi044@abv.bg>
2020-08-07 17:24:03 +0300

    Add first method from import task

--- EntityFrameworkCore/ExamPreparation/C#DBAdvancedExam-07Apr2019/Cinema/Cinema/DataProcessor/Deserializer.cs
+++ EntityFrameworkCore/ExamPreparation/C#DBAdvancedExam-07Apr2019/Cinema/Cinema/DataProcessor/Deserializer.cs
@@ -1,8 +1,22 @@
 ﻿namespace Cinema.DataProcessor
 {
     using System;
-
-    using Data;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+    using System.Xml.Serialization;
+    using AutoMapper;
+    using Cinema.Data;
+    using Cinema.Data.Models;
+    using Cinema.Data.Models.Enums;
+    using Cinema.DataProcessor.ImportDto;
+    using Microsoft.EntityFrameworkCore.Internal;
+    using Newtonsoft.Json;
+    using ValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
 
     public class Deserializer
     {
@@ -18,7 +32,34 @@
 
         public static string ImportMovies(CinemaContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            var importMovies = JsonConvert.DeserializeObject<List<ImportMovieDto>>(jsonString);
+
+            StringBuilder sb = new StringBuilder();
+
+            List<Movie> movies = new List<Movie>();
+
+            foreach (var movie in importMovies)
+            {
+                bool doesMovieExist = movies.Any(m => m.Title == movie.Title);
+                bool isValid = IsValid(movie);
+                var isValidEnum = Enum.TryParse(typeof(Genre), movie.Genre, out object genre);
+
+                if (doesMovieExist || !isValid || !isValidEnum)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Movie movieToAdd = Mapper.Map<Movie>(movie);
+
+                movies.Add(movieToAdd);
+                sb.AppendLine(string.Format(SuccessfulImportMovie, movie.Title, movie.Genre, movie.Rating.ToString("F2")));
+            }
+
+            context.Movies.AddRange(movies);
+            context.SaveChanges();
+
+            return sb.ToString();
         }
 
         public static string ImportHallSeats(CinemaContext context, string jsonString)
@@ -34,6 +75,14 @@
         public static string ImportCustomerTickets(CinemaContext context, string xmlString)
         {
             throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
         }
     }
 }
