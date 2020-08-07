155ed7c0528e0c1aa038298951de51fc37df709a
slavi044 <slavi044@abv.bg>
2020-08-07 12:43:30 +0300

    Add json export method

--- EntityFrameworkCore/ExamPreparation/C#DBAdvancedExam-07Dec2019/TeisterMask/DataProcessor/Serializer.cs
+++ EntityFrameworkCore/ExamPreparation/C#DBAdvancedExam-07Dec2019/TeisterMask/DataProcessor/Serializer.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using Data;
-    using Formatting = Newtonsoft.Json.Formatting;
+    using System.Linq;
+    using System.Globalization;
+    using Newtonsoft.Json;
 
     public class Serializer
     {
@@ -11,9 +13,45 @@
             throw new NotImplementedException();
         }
 
+        /*Select the top 10 employees who have at least one task that its open date is after or equal 
+         * to the given date with their tasks that meet the same requirement 
+         * (to have their open date after or equal to the giver date).
+         * For each employee, export their username and their tasks. For each task,
+         * export its name and open date (must be in format "d"), 
+         * due date (must be in format "d"), label and execution type. 
+         * Order the tasks by due date (descending), then by name (ascending). 
+         * Order the employees by all tasks count (descending), then by username (ascending).*/
+
         public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var employees = context
+              .Employees
+              .Select(e => new
+              {
+                  Username = e.Username,
+                  Tasks = e.EmployeesTasks
+                      .Where(t => t.Task.OpenDate >= date)
+                      .OrderByDescending(t => t.Task.DueDate)
+                      .ThenBy(t => t.Task.Name)
+                      .Select(t => new
+                      {
+                          TaskName = t.Task.Name,
+                          OpenDate = t.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture),
+                          DueDate = t.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                          LabelType = t.Task.LabelType.ToString(),
+                          ExecutionType = t.Task.ExecutionType.ToString()
+                      })
+                      .ToList()
+              })
+              .ToList()
+              .OrderByDescending(e => e.Tasks.Count)
+              .ThenBy(e => e.Username)
+              .Take(10)
+              .ToList();
+
+            string json = JsonConvert.SerializeObject(employees, Formatting.Indented);
+
+            return json;
         }
     }
 }
